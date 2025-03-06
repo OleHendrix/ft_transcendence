33afae4da22f2bc820235b65d21681c9d509ecf7
Ole Hendrix <160501406+OleHendrix@users.noreply.github.com>
2025-03-06 12:05:48 +0100

    hero section

--- frontend/src/Hero.tsx
+++ frontend/src/Hero.tsx
@@ -1,12 +1,22 @@
+import { RiGamepadLine } from "react-icons/ri";
 
 
 function Hero()
 {
 	return(
 		<div className="w-full h-220 flex justify-between">
-			<div className="w-1/2 flex items-center justify-center flex-col p-24 space-y-8">
-				<h1 className="text-5xl  font-normal text-brand-orange">Are you ready for an <span className="font-black italic text-[#ff914d]">transcending</span> game of Pong?</h1>
+			<div className="w-1/2 flex justify-center flex-col p-24 space-y-8">
+				<h1 className="text-5xl  font-semibold text-brand-orange">Are you ready for an <span className="font-black italic text-[#ff914d]">transcending</span> game of Pong?</h1>
 				<p className="text-xl">Get ready for the ultimate Pong experience. Challenge your friends in fast-paced, competitive matches where every point matters. Are you ready to outplay, outlast, and outscore?</p>
+				<div className="flex justify-start space-x-4 font-bold text-lg">
+				    <button className="flex items-center space-x-2 bg-[#ff914d] hover:bg-[#ab5a28] text-white py-1 px-4 rounded-3xl shadow-lg">
+						<p>Login</p>
+        			</button>
+					<button className="flex items-center space-x-2 bg-[#134588] hover:bg-[#246bcb] text-white py-2 px-4 rounded-3xl">
+						<p>Play</p>
+						<RiGamepadLine />
+					</button>
+				</div>
 			</div>
 		</div>
 	)
