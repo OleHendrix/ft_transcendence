79c57709d83f614938c44bb07e99d303c0090ced
Alijn Kuijer <akuijer@f1r3s14.codam.nl>
2025-03-18 18:06:27 +0100

    add against AI support

--- frontend/src/PongGame.tsx
+++ frontend/src/PongGame.tsx
@@ -18,6 +18,19 @@ interface Ball {
 	dir:		Vec2;
 }
 
+interface AI {
+	enabled:		boolean;
+	lastActivation:	number;
+	desiredY:		number;
+}
+
+interface Statics {
+	BOUNCE:		Vec2;
+	CARRYOVER:	number,
+	VELOCITY:	number,
+	FRICTION:	number,
+}
+
 function PongGame()
 {
 	const [p1Y, setP1Y] = useState(50);
@@ -30,6 +43,19 @@ function PongGame()
 		prevPos:	{ x: 50,  y: 50  },
 		size:		{ x: 2,   y: 2   },
 		dir:		{ x: 0.5, y: 0.5 },
+	});
+
+	const [ai, setAI] = useState<AI>({
+		enabled: false,
+		lastActivation: 0,
+		desiredY: 50
+	});
+
+	const [s] = useState<Statics>({
+		BOUNCE:		{ x: -1.02, y: -0.9 },
+		CARRYOVER:	0.5,
+		VELOCITY:	0.3,
+		FRICTION:	0.9,
 	});
 
 	const [p1Score, setP1Score] = useState(0);
@@ -50,7 +76,7 @@ function PongGame()
 		{
 			setBall(prev => ({ ...prev,
 				pos: { ...prev.pos, y: prev.pos.y <= 1 ? 2 : 98 },
-				dir: { ...prev.dir, y: prev.dir.y * -0.9 },
+				dir: { ...prev.dir, y: prev.dir.y * s.BOUNCE.y },
 			}));
 		}
 
@@ -58,14 +84,14 @@ function PongGame()
 		{
 			setBall(prev => ({ ...prev,
 				pos: { ...prev.pos, x: 5 },
-				dir: { x: prev.dir.x * -1.02, y: prev.dir.y + p1DirY * 0.5 },
+				dir: { x: prev.dir.x * s.BOUNCE.x, y: prev.dir.y + p1DirY * s.CARRYOVER },
 			}));
 		}
 		if (ball.pos.x > 95 && ball.prevPos.x <= 95 && ball.pos.y >= p2Y - 10 && ball.pos.y <= p2Y + 10)
 		{
 			setBall(prev => ({ ...prev,
 				pos: { ...prev.pos, x: 95 },
-				dir: { x: prev.dir.x * -1.02, y: prev.dir.y + p2DirY * 0.5 },
+				dir: { x: prev.dir.x * s.BOUNCE.x, y: prev.dir.y + p2DirY * s.CARRYOVER },
 			}));
 		}
 
@@ -81,11 +107,40 @@ function PongGame()
 		}
 	}
 
+	function manageAI()
+	{
+		if (!ai.enabled)
+			return;
+		const timeSeconds = Math.floor(Date.now() / 1000);
+
+		if (ai.lastActivation === timeSeconds)
+			return;
+		ai.lastActivation = timeSeconds;
+		const ballCopy = structuredClone(ball);
+		while (ballCopy.pos.x < 95)
+		{
+			if (ballCopy.pos.y <= 1 || ballCopy.pos.y >= 99)
+			{
+				ballCopy.pos.y = ballCopy.pos.y <= 1 ? 2 : 98;
+				ballCopy.dir.y *= s.BOUNCE.y;
+			}
+			if (ballCopy.pos.x < 5)
+			{
+				ballCopy.pos.x = 5;
+				ballCopy.dir.x *= s.BOUNCE.x;
+				// ballCopy.dir.y += p1DirY * 0.5;
+			}
+			ballCopy.pos.x += ballCopy.dir.x;
+			ballCopy.pos.y += ballCopy.dir.y;
+		}
+		ai.desiredY = ballCopy.pos.y;
+	}
+
 	function managePaddle(dirY: number, setPosY: React.Dispatch<React.SetStateAction<number>>, setDirY: React.Dispatch<React.SetStateAction<number>>) {
 		const offset: number = 10;
 		setPosY(prevPosY => {
 			if (prevPosY < offset || prevPosY > 100 - offset) {
-				setDirY(0); // Reset direction
+				setDirY(0);
 				return Math.max(offset, Math.min(100 - offset, prevPosY + dirY));
 			}
 			return prevPosY + dirY;
@@ -96,8 +151,16 @@ function PongGame()
 
 	useEffect(() =>
 	{
-		const handleKeyDown = (e: KeyboardEvent) => {setKeysPressed(prev => ({ ...prev, [e.key]: true }));};
-		const handleKeyUp = (e: KeyboardEvent) => {setKeysPressed(prev => ({ ...prev, [e.key]: false }));};
+		const handleKeyDown = (e: KeyboardEvent) => {
+			setKeysPressed(prev => ({ ...prev, [e.key]: true }));
+			if (e.key === "t" || e.key === "T") {
+				setAI(prevAI => ({ ...prevAI, enabled: !prevAI.enabled }));
+			}
+		};
+
+		const handleKeyUp = (e: KeyboardEvent) => {
+			setKeysPressed(prev => ({ ...prev, [e.key]: false }));
+		};
 
 		window.addEventListener('keydown', handleKeyDown);
 		window.addEventListener('keyup', handleKeyUp);
@@ -109,9 +172,11 @@ function PongGame()
 	{
 		const moveFrame = () =>
 		{
-			setP1DirY(P1DirY => 0.9 * P1DirY + 0.2 * (keysPressed['w'] && keysPressed['s'] ? 0 : (keysPressed['w'] ? -1 : 0) + (keysPressed['s'] ? 1 : 0)));
-			setP2DirY(P2DirY => 0.9 * P2DirY + 0.2 * (keysPressed['ArrowUp'] && keysPressed['ArrowDown'] ? 0 : (keysPressed['ArrowUp'] ? -1 : 0) + (keysPressed['ArrowDown'] ? 1 : 0)));
-
+			setP1DirY(P1DirY => s.FRICTION * P1DirY + s.VELOCITY * (keysPressed['w'] && keysPressed['s'] ? 0 : (keysPressed['w'] ? -1 : 0) + (keysPressed['s'] ? 1 : 0)));
+			if (ai.enabled === false)
+				setP2DirY(P2DirY => s.FRICTION * P2DirY + s.VELOCITY * (keysPressed['ArrowUp'] && keysPressed['ArrowDown'] ? 0 : (keysPressed['ArrowUp'] ? -1 : 0) + (keysPressed['ArrowDown'] ? 1 : 0)));
+			else
+				setP2DirY(P2DirY => s.FRICTION * P2DirY + s.VELOCITY * ((p2Y + 8 < ai.desiredY) ? 1 : (p2Y - 8 > ai.desiredY) ? -1 : 0));
 			managePaddle(p1DirY, setP1Y, setP1DirY);
 			managePaddle(p2DirY, setP2Y, setP2DirY);
 
@@ -120,7 +185,8 @@ function PongGame()
 				pos:     { x: prev.pos.x + prev.dir.x, y: prev.pos.y + prev.dir.y },
 			}));
 
-			handleColision()
+			manageAI();
+			handleColision();
 			
 			animationId = requestAnimationFrame(moveFrame);
 		};
