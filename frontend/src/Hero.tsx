9d1a87b8cf64249f38e90dd2e6e60230469e31c4
Alijn Kuijer <alijnkuijer@gmail.com>
2025-03-31 14:21:10 +0200

    change start game layout in hero

--- frontend/src/Hero.tsx
+++ frontend/src/Hero.tsx
@@ -2,6 +2,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { RiGamepadLine } from "react-icons/ri";
 import { TbTournament } from "react-icons/tb";
 import { RiRobot2Line } from "react-icons/ri";
+import { LuCable } from "react-icons/lu";
 import { useAccountContext } from "./contexts/AccountContext";
 import Players from "./Players";
 import "./css/ponganimation.css";
@@ -27,38 +28,55 @@ function AddGame()
 function Hero()
 {
 	const { loggedInAccounts, setIsPlaying } = useAccountContext();
+	const hoverScale = 1.03;
+	const tapScale = 0.97;
 
 	return(
 		<div className="w-full flex justify-between items-start pt-[10vh]">
 			<div className="w-1/2 flex h-[calc(100vh - 8vh)] justify-start flex-col p-24 pr-16 space-y-12 px-[6vw]">
 				<h1 className="text-6xl  font-semibold text-brand-orange">Are you ready for a <span className="font-black italic text-[#ff914d]">transcending</span> game of Pong?</h1>
 				<p className="text-2xl">Get ready for the ultimate Pong experience. Challenge your friends in fast-paced, competitive matches where every point matters. Are you ready to outplay, outlast, and outscore?</p>
-				<div className="flex justify-start space-x-4 font-bold text-lg">
-					<motion.button className={`flex items-center h-10 space-x-2 bg-[#134588] text-white py-2 px-4 rounded-3xl
-						${loggedInAccounts.length !== 2 ? 'opacity-40' : 'hover:bg-[#246bcb] hover:cursor-pointer'}`} 
-						whileHover={(loggedInAccounts.length === 2 ? {scale: 1.03} : {})} 
-						whileTap={(loggedInAccounts.length === 2 ? {scale: 0.97} : {})}
+				<div className="flex flex-col items-start space-y-2 font-bold text-lg">
+					<p className="text-s text-lg font-medium">1 Player:</p>
+					<div className="flex flex-row space-x-4">
+						<motion.button className={`flex items-center h-10 space-x-2 bg-[#134588] text-white px-4 py-0 rounded-3xl w-auto
+						${loggedInAccounts.length !== 1 ? 'opacity-40' : 'hover:bg-[#246bcb] hover:cursor-pointer'}`}
+							whileHover={(loggedInAccounts.length === 1 ? { scale: hoverScale } : {})}
+							whileTap={(loggedInAccounts.length === 1 ? { scale: tapScale } : {})}
+							onClick={() => setIsPlaying(true)}>
+							<p>Versus AI</p>
+							<RiRobot2Line />
+						</motion.button>
+
+						<motion.button className={`flex items-center h-10 space-x-2 bg-[#134588] text-white px-4 py-0 rounded-3xl w-auto
+						${loggedInAccounts.length !== 1 ? 'opacity-40' : 'hover:bg-[#246bcb] hover:cursor-pointer'}`}
+							whileHover={(loggedInAccounts.length === 1 ? { scale: hoverScale } : {})}
+							whileTap={(loggedInAccounts.length === 1 ? { scale: tapScale } : {})}>
+							<p>Online Game</p>
+							<LuCable />
+						</motion.button>
+					</div>
+
+					<p className="text-s text-lg font-medium">2 Players:</p>
+					<motion.button className={`flex items-center h-10 space-x-2 bg-[#134588] text-white px-4 py-0 rounded-3xl w-auto
+					${loggedInAccounts.length !== 2 ? 'opacity-40' : 'hover:bg-[#246bcb] hover:cursor-pointer'}`}
+						whileHover={(loggedInAccounts.length === 2 ? { scale: hoverScale } : {})}
+						whileTap={(loggedInAccounts.length === 2 ? { scale: tapScale } : {})}
 						onClick={() => setIsPlaying(true)}>
-						<p>Play</p>
+						<p>Local Game</p>
 						<RiGamepadLine />
 					</motion.button>
-					<motion.button className={`flex items-center h-10 space-x-2 bg-[#134588] text-white py-2 px-4 rounded-3xl
-						${loggedInAccounts.length !== 1 ? 'opacity-40' : 'hover:bg-[#246bcb] hover:cursor-pointer'}`} 
-						whileHover={(loggedInAccounts.length === 1 ? {scale: 1.03} : {})} 
-						whileTap={(loggedInAccounts.length === 1 ? {scale: 0.97} : {})}
-						onClick={() => setIsPlaying(true)}>
-						<p>Play vs AI</p>
-						<RiRobot2Line />
-					</motion.button>
-					<motion.button className={`flex items-center h-10 space-x-2 bg-[#ff914d] text-white py-2 px-4 rounded-3xl
-						${loggedInAccounts.length < 3 ? 'opacity-40' : 'hover:bg-[#ab5a28] hover:cursor-pointer'}`}
-						whileHover={(loggedInAccounts.length > 2 ? {scale: 1.03} : {})}
-						whileTap={(loggedInAccounts.length > 2 ? {scale: 0.97} : {})}>
+
+					<p className="text-s text-lg font-medium">3+ Players:</p>
+					<motion.button className={`flex items-center h-10 space-x-2 bg-[#ff914d] text-white px-4 py-0 rounded-3xl w-auto
+					${loggedInAccounts.length < 3 ? 'opacity-40' : 'hover:bg-[#ab5a28] hover:cursor-pointer'}`}
+						whileHover={(loggedInAccounts.length > 2 ? { scale: hoverScale } : {})}
+						whileTap={(loggedInAccounts.length > 2 ? { scale: tapScale } : {})}>
 						<p>Tournament</p>
 						<TbTournament />
 					</motion.button>
 				</div>
-					<Players />
+				{/* <Players /> */}
 			</div>
 			<div className="w-1/2 h-[calc(100vh - 8vh)] flex justify-start flex-col p-24 pl-16 space-y-12 max-w-[50vw] px-[6vw]">
 				<SimplePong />
