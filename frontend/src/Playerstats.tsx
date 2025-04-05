ebe89ecaec9b8b93432339187b925f559cabfe13
LasseThuis <kruimelkoning@gmail.com>
2025-04-05 14:29:33 +0200

    changed the way of fetching accounts for leaderboard

--- frontend/src/Playerstats.tsx
+++ frontend/src/Playerstats.tsx
@@ -1,57 +1,27 @@
-import axios from "axios";
 import { useAccountContext } from "./contexts/AccountContext";
 import { useLoginContext } from "./contexts/LoginContext";
-import { useEffect, useState } from 'react';
-
-interface	Stats
-{
-	wins:	number;
-	draws:	number;
-	losses:	number;
-	elo:	number;
-}
 
 function PlayerStats()
 {
 	const { loggedInAccounts } = useAccountContext();
-	const { indexPlayerStats, showPlayerStats } = useLoginContext();
-	const [ stats, setStats ]  = useState<Stats>();
-
-	useEffect(() =>
-	{
-		async function getStats()
-		{
-			try
-			{
-				const response = await axios.post(`http://${window.location.hostname}:5001/api/get-stats`,
-					{ userId: loggedInAccounts[indexPlayerStats].id });
-				const playerStats = response.data as Stats;
-				setStats(playerStats);
-				console.log('stats', stats, 'data', response.data);
-			}
-			catch (error)
-			{
-				console.error('failed to fetch Stats', error);
-			}
-		} getStats();
-	}, [ showPlayerStats ]);
+	const { indexPlayerStats } = useLoginContext();
 
 	return (
 		<div className="flex flex-col items-center mt-4">
 			<h2 className="text-2xl font-bold text-center">Stats</h2>
-			<p className='stat-title mt-2 font-black text-xl'>{stats?.elo}</p>
+			<p className='stat-title mt-2 font-black text-xl'>{loggedInAccounts[indexPlayerStats]?.elo}</p>
 			<div className="w-full grid grid-cols-3 gap-2 p-2 mt-2">
 				<div className="stat flex flex-col items-center">
 					<div className="stat-title text-green-800 font-black">Wins</div>
-					<div className="stat-value">{stats?.wins}</div>
+					<div className="stat-value">{loggedInAccounts[indexPlayerStats]?.wins}</div>
 				</div>
 				<div className="stat flex flex-col items-center">
 					<div className="stat-title font-black">Draws</div>
-					<div className="stat-value">{stats?.draws}</div>
+					<div className="stat-value">{loggedInAccounts[indexPlayerStats]?.draws}</div>
 				</div>
 				<div className="stat flex flex-col items-center">
 					<div className="stat-title text-red-800 font-black">Losses</div>
-					<div className="stat-value">{stats?.losses}</div>
+					<div className="stat-value">{loggedInAccounts[indexPlayerStats]?.losses}</div>
 				</div>
 			</div>
 		</div>
