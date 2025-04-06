cfc8ea893c19043461e24f51748b7b15f2d983a3
Alijn Kuijer <alijnkuijer@gmail.com>
2025-04-06 18:34:43 +0200

    improve leaderboard

--- frontend/src/Leaderboard.tsx
+++ frontend/src/Leaderboard.tsx
@@ -4,58 +4,92 @@ import { PlayerType } from './types';
 import axios from 'axios';
 import { useAccountContext } from './contexts/AccountContext';
 import { IoMdClose } from 'react-icons/io';
+import { GoTrophy } from "react-icons/go";
 
-export default function Leaderboard ()
-{
-	const { accounts, showLeaderboard, setShowLeaderboard } = useAccountContext();
-	const [ sortedAccounts, setSortedAccounts ] = useState<PlayerType[]>([]);
+export default function Leaderboard() {
+	const { accounts, setShowLeaderboard } = useAccountContext();
+	const [sortedAccounts, setSortedAccounts] = useState<PlayerType[]>([]);
 
-	useEffect(() =>
-	{
+	useEffect(() => {
 		setSortedAccounts(accounts.sort((a, b) => b.elo - a.elo));
-	}, [ showLeaderboard ]);
+	}, []);
+
+	function getBorderColour(position: number): string
+	{
+		if (position > 3)
+			return "";
+	
+		let str = "shadow-xl text-lg border-2 ";
+	
+		switch (position) { 
+			case 1: str += "bg-gradient-to-r from-[#C88F0B] via-[#E0B320] to-[#F0D000] border-[#C88F0B]"; break;
+			case 2: str += "bg-gradient-to-r from-[#B0B0B0] via-[#D0D0D0] to-[#E0E0E0] border-[#B0B0B0]"; break;
+			case 3: str += "bg-gradient-to-r from-[#B85C00] via-[#DD7500] to-[#E08400] border-[#B85C00]"; break;
+
+		}
+		return str;
+	}
 
 	return (
 		<AnimatePresence>
-			<motion.div 
+			<motion.div
 				className="fixed inset-0 backdrop-blur-sm flex items-center justify-center z-50 bg-[#1a1a1a]/90"
-				initial={{ opacity: 0 }} 
-				animate={{ opacity: 1 }} 
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
 				exit={{ opacity: 0 }}
 			>
-				<motion.div 
+				<motion.div
 					className="flex flex-col items-center bg-[#2a2a2a]/90 text-white p-8 gap-8 rounded-lg w-full min-w-[400px] min-h-[500px] max-w-xl max-h-[600px] relative shadow-xl"
-					initial={{ scale: 0.9, y: 20 }} 
-					animate={{ scale: 1, y: 0 }} 
-					exit={{ scale: 0.9, y: 20 }} 
+					initial={{ scale: 0.9, y: 20 }}
+					animate={{ scale: 1, y: 0 }}
+					exit={{ scale: 0.9, y: 20 }}
 					transition={{ type: "spring", stiffness: 300, damping: 25 }}
 				>
-					<h1 className="justify-center text-xl font-black">Leaderboard</h1>
-					<button 
+					<h1 className="flex justify-center text-4xl gap-2 font-medium font-black items-center">
+						Leaderboard <GoTrophy />
+					</h1>
+					<button
 						className="absolute top-4 right-4 text-gray-400 hover:text-white hover:cursor-pointer"
 						onClick={() => setShowLeaderboard(false)}
 					>
 						<IoMdClose size={24} />
 					</button>
-	
+
 					<div className="w-full h-80 overflow-y-auto rounded-lg border border-base-content/5 bg-transparent">
-						<table className="table w-full">
+						<table className="table w-full text-center">
 							<thead>
-								<tr className="bg-[#3a3a3a]/90 text-white">
-									<th>#</th>
-									<th>Name</th>
-									<th>ELO</th>
+								<tr className="text-lg font-light bg-[#303030]/90 text-lightgrey">
+									<th className="font-[450] text-left">#</th>
+									<th className="font-[450] text-left">Name</th>
+									<th className="font-[450]">ELO</th>
+									<th className="font-[450]">Wins</th>
+									<th className="font-[450]">Losses</th>
+									<th className="font-[450]">Win Rate</th>
 								</tr>
 							</thead>
 							<tbody>
 								{sortedAccounts.map((account, index) => (
-									<tr 
-										key={account.id} 
-										className={index % 2 === 0 ? "bg-[#3a3a3a]/80" : "bg-[#454545]/80"}
+									<tr
+										key={account.id}
+										className={
+											index === 0 ? "bg-[linear-gradient(to_right,_#FFD70080_0%,_#FFD70032_15%,_#E0B32022_25%,_#F0D00001_100%)]" :
+											index === 1 ? "bg-[linear-gradient(to_right,_#C0C0D080_0%,_#C0C0D032_15%,_#C0C0D022_25%,_#C0C0D001_100%)]" :
+											index === 2 ? "bg-[linear-gradient(to_right,_#B85C0080_0%,_#B85C0032_15%,_#B85C0022_25%,_#B85C0001_100%)]" :
+											index % 2 === 0 ? "bg-[#303030]/80" : "bg-[#383838]/80"}
 									>
-										<th>{index + 1}</th>
-										<td>{account.username}</td>
-										<td>{account.elo}</td>
+
+										<td className="w-6 p-[0.25px]">
+											<div className={`w-9 h-9 rounded-md ml-1 flex items-center justify-center 
+												${getBorderColour(index + 1)}`}
+												style={{ textShadow: '1px 1px 4px rgba(0, 0, 0, 0.5)' }}>
+												{index + 1}
+											</div>
+										</td>
+										<td className="text-left">{account.username}</td>
+										<td className="w-25">{account.elo}</td>
+										<td className="w-25">{account.wins}</td>
+										<td className="w-25">{account.losses}</td>
+										<td className="w-25">{(account.wins + account.losses === 0) ? "-" : account.wins / (account.wins + account.losses) * 100 + '%'}</td>
 									</tr>
 								))}
 							</tbody>
@@ -65,5 +99,5 @@ export default function Leaderboard ()
 			</motion.div>
 		</AnimatePresence>
 	);
-	
+
 };
