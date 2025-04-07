9e199fa2d0a62ad4da99c2a8c03ebbfcd79aec38
Alijn Kuijer <alijnkuijer@gmail.com>
2025-04-08 00:14:15 +0200

    improve stat page UI, fucked up exit button D:

--- frontend/src/user/Playerstats.tsx
+++ frontend/src/user/Playerstats.tsx
@@ -15,67 +15,46 @@ function ShowMatchHistory()
 	const selectedAccount = loggedInAccounts?.[indexPlayerStats];
 	const account = accounts.find(acc => acc.id === selectedAccount?.id);
 	const matchHistory = account?.matches ?? [];
-	const sortedMatchHistory = [...matchHistory].sort((a, b) => b.id - a.id);
+	const SMD = [...matchHistory].sort((a, b) => b.id - a.id); //shortenedMatchHistory
 
 	console.log(matchHistory);
 	console.log("Accounts:", accounts);
 	console.log("LoggedIn:", loggedInAccounts);
 	return (
-		<div className="w-full h-80 overflow-y-auto rounded-lg border border-base-content/5 bg-transparent">
-			<table className="table w-full text-center">
-				<thead>
-					<tr className="text-lg font-light bg-[#303030]/90 text-lightgrey">
-						<th className="w-100 text-center" colSpan={6}>Match History</th>
-					</tr>
-				</thead>
-				<tbody>
-					{sortedMatchHistory.map((match, index) => (
-						<tr
-							key={match.id}
-							className={
-								match.winner === match.p1 ? "bg-gradient-to-r from-[#2c8a39] to-[#1f4b28]" : "bg-gradient-to-r from-[#e02e2e] to-[#8b1313]"}
-						>
-							<td className="text-left">{sortedMatchHistory[index].p1}</td>
-							<td className="text-left text-xs">
-								{`${sortedMatchHistory[index].p1Elo} (${sortedMatchHistory[index].p1Diff >= 0 ? `+${sortedMatchHistory[index].p1Diff}` : sortedMatchHistory[index].p1Diff})`}
-							</td>
-							<td className="text-right text-[#ff914d]">{sortedMatchHistory[index].p1score}</td>
-							<td className="text-left  text-[#134588]">{sortedMatchHistory[index].p2score}</td>
-							<td className="text-right  text-xs">
-								{`${sortedMatchHistory[index].p2Elo} (${sortedMatchHistory[index].p2Diff >= 0 ? `+${sortedMatchHistory[index].p2Diff}` : sortedMatchHistory[index].p1Diff})`}
-							</td>
-							<td className="text-right">{sortedMatchHistory[index].p2}</td>
+		<div className="w-full h-117 overflow-y-auto border border-base-content/5 bg-transparent">
+			<div className="overflow-x-auto max-w-full">  {/* Add this wrapper div */}
+				<table className="table w-full max-w-full text-center">
+					<thead>
+						<tr className="text-lg font-light bg-[#303030]/90 text-lightgrey">
+							<th className="w-100 text-center" colSpan={6}>Match History</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						{SMD.map((match, index) => (
+							<tr
+								key={match.id}
+								// TODO: add support for ties
+								className={`${"font-medium "} ${match.winner === match.p1
+									? "bg-[linear-gradient(to_bottom_right,_#2c8a3950_0%,_#20602f90_30%,_#0f402470_70%,_#1f4b2837_100%)]"
+									: "bg-[linear-gradient(to_bottom_right,_#e02e2e50_0%,_#aa202090_30%,_#8b131370_70%,_#8b131337_100%)]"}`}
+								style={{ height: '69px' }}
+							>
+								<td className="text-left text-2xl pl-3">     		   		{SMD[index].p1}</td>
+								<td className="italic text-gray-300 text-xs pr-4">			{`${SMD[index].p1Elo} (${SMD[index].p1Diff >= 0 ? `+${SMD[index].p1Diff}` : SMD[index].p1Diff})`}</td>
+								<td className="text-center text-2xl font-bold flex-grow">	{SMD[index].p1score}-{SMD[index].p2score}</td>
+								<td className="italic text-gray-300 text-xs pl-4">			{`${SMD[index].p2Elo} (${SMD[index].p2Diff >= 0 ? `+${SMD[index].p2Diff}` : SMD[index].p1Diff})`}</td>
+								<td className="text-right text-2xl pr-3">     				{SMD[index].p2}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
 		</div>
-	)
+	);
 }
 
-function PlayerStats()
+function ShowStats()
 {
-	const { accounts, loggedInAccounts } = useAccountContext();
-	const { indexPlayerStats, showPlayerStats } = useLoginContext();
-	const [ index, setIndex ]  = useState(-1);
-	const [ showStats, setShowStats ] = useState(false);
-
-	useEffect(() =>
-	{
-		setIndex(loggedInAccounts[indexPlayerStats].id - 2);
-	}, [ showPlayerStats ]);
-
-	return (
-		<>
-			<motion.button className="pt-2 bg-[#ff914d] px-4 py-2 font-bold shadow-2xl rounded-3xl hover:bg-[#ab5a28] hover:cursor-pointer"
-				whileHover={{ scale: 1.03 }}
-				whileTap={{ scale: 0.97 }}
-				onClick={() => { setShowStats(true) }}>Show stats
-			</motion.button>
-			{showStats && <StatWindow />}
-		</>
-	)
-
 	function calcWorseThan(player: PlayerType, stat: keyof PlayerType): [number, number]
 	{
 		let worseThan = 0;
@@ -131,123 +110,131 @@ function PlayerStats()
 		};
 	}
 
-	function StatWindow()
+	function GetStatEntry(isEven: boolean, startStr: string, unit: string, player: PlayerType, stat: keyof PlayerType): React.ReactElement
 	{
-		const { accounts, loggedInAccounts } = useAccountContext();
-		const { indexPlayerStats, showPlayerStats } = useLoginContext();
-		const [ index, setIndex ]  = useState(-1);
-		const [profileImage, setProfileImage] = useState(Player);
-
-		const currentAccount = accounts[loggedInAccounts[indexPlayerStats].id - 2]; //findAccount(indexPlayerStats);
-		if (currentAccount === null)
-			return;
-
 		return (
-			<AnimatePresence>
-				<motion.div
-					className="fixed inset-0 backdrop-blur-sm flex items-center justify-center z-50 bg-[#1a1a1a]/90"
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					exit={{ opacity: 0 }}
-				>
-					<motion.div
-						className="flex-col items-center bg-[#2a2a2a]/90 backdrop-blur-md text-white p-8 gap-8 rounded-xl w-full min-w-[400px] min-h-[700px] max-w-xl max-h-[700px] relative shadow-2xl"
-						initial={{ scale: 0.9, y: 20 }}
-						animate={{ scale: 1, y: 0 }}
-						exit={{ scale: 0.9, y: 20 }}
-						transition={{ type: "spring", stiffness: 300, damping: 25 }}
-					>
-						<button
-							className="absolute top-4 right-4 text-gray-400 hover:text-white hover:bg-neutral-700 p-1 rounded transition"
-							onClick={() => setShowStats(false)}
-						>
-							<IoMdClose size={24} />
-						</button>
-
-						<header className="relative flex items-center justify-center gap-x-2 text-5xl mb-4 border-b border-neutral-700 pb-4">
-							{currentAccount?.username}
-							<img
-								src={profileImage}
-								className="h-14 w-14 rounded-full object-cover shadow-md hover:scale-105 transition-transform duration-300"
-							/>
-						</header>
-
-						<main className="flex h-full gap-x-4 text-2xl text-center">
-							<div className="w-2/4">
-								<div className="flex flex-col gap-y-4 mb-6">
-									<div className="w-full grid grid-cols-3 gap-2 p-2 -mt-4 -mb-4">
-										<div className="stat flex flex-col items-center">
-											<div className="stat-title text-green-800 font-black">Wins</div>
-											<div className="stat-value">{currentAccount?.wins}</div>
-										</div>
-										<div className="stat flex flex-col items-center">
-											<div className="stat-title font-black">Draws</div>
-											<div className="stat-value">{currentAccount?.draws}</div>
-										</div>
-										<div className="stat flex flex-col items-center">
-											<div className="stat-title text-red-800 font-black">Losses</div>
-											<div className="stat-value">{currentAccount?.losses}</div>
-										</div>
-									</div>
-
-									<div
-										className="flex flex-col p-3 rounded-md shadow-inner backdrop-blur-sm border border-neutral-800"
-										style={{ ...GetGradientStyle(currentAccount?.elo, 800) }}
-									>
-										<div className="text-2xl font-medium">Rating: {currentAccount?.elo}</div>
-										<div className="italic text-xs font-medium text-gray-400">
-											{getPercentile(currentAccount, 'elo')}
-										</div>
-									</div>
-
-									<div
-										className="flex flex-col p-3 rounded-md shadow-inner backdrop-blur-sm border border-neutral-800"
-										style={{ ...GetGradientStyle(currentAccount?.winRate, 100) }}
-									>
-										<div className="text-2xl font-medium">
-											Win rate: {currentAccount?.winRate === null ? '-' : toPercentage(currentAccount?.winRate, 1) + "%"}
-										</div>
-										<div className="italic text-xs font-medium text-gray-400">
-											{getPercentile(currentAccount, 'winRate')}
-										</div>
-									</div>
-									
-
-									<div
-										className="flex flex-col p-3 rounded-md shadow-inner backdrop-blur-sm border border-neutral-800"
-										style={{ ...GetGradientStyle(currentAccount?.winRate, 100) }}
-									>
-										<div className="text-2xl font-medium">
-											Win rate: {currentAccount?.winRate === null ? '-' : toPercentage(currentAccount?.winRate, 1) + "%"}
-										</div>
-										<div className="italic text-xs font-medium text-gray-400">
-											{getPercentile(currentAccount, 'winRate')}
-										</div>
-									</div>
-
-									<div
-										className="flex flex-col p-3 rounded-md shadow-inner backdrop-blur-sm border border-neutral-800"
-										style={{ ...GetGradientStyle(currentAccount?.winRate, 100) }}
-									>
-										<div className="text-2xl font-medium">
-											Win rate: {currentAccount?.winRate === null ? '-' : toPercentage(currentAccount?.winRate, 1) + "%"}
-										</div>
-										<div className="italic text-xs font-medium text-gray-400">
-											{getPercentile(currentAccount, 'winRate')}
-										</div>
-									</div>
-								</div>
-							</div>
-
-							<div className="w-1/2">
-								<ShowMatchHistory />
-							</div>
-						</main>
-					</motion.div>
-				</motion.div>
-			</AnimatePresence>
+			<tr className={`${isEven ? "bg-[#303030]/80" : "bg-[#383838]/80"}`}>
+				<td className="text-left text-2xl font-medium pl-2" style={{ height: '45px' }}>
+					{startStr}
+				</td>
+				<td className="text-right p-2">
+					<div className="text-3xl font-semibold font-mono" style={{ fontFamily: '"Droid Sans Mono", monospace' }}>{unit === "%" ? toPercentage(player[stat] as number, 0).toString() : player[stat].toString()}{unit}</div>
+					<div className="text-xs italic text-gray-400">{getPercentile(player, stat)}</div>
+				</td>
+			</tr>
 		);
 	}
+	
+
+	const { accounts, loggedInAccounts } = useAccountContext();
+	const { indexPlayerStats, showPlayerStats } = useLoginContext();
+
+	const currentAccount = accounts[loggedInAccounts[indexPlayerStats].id - 2];
+	if (currentAccount === null)
+		return;
+
+	return (
+		<div className="w-full overflow-y-auto border border-base-content/5 bg-transparent">
+			<link href="https://fonts.googleapis.com/css2?family=Droid+Sans+Mono:wght@400;500;600&display=swap" rel="stylesheet"></link>
+			<table className="table w-full">
+				<thead>
+					<tr className="text-lg font-light bg-[#303030]/90 text-lightgrey">
+						<th className="w-100 text-center" colSpan={6}>Stats</th>
+					</tr>
+				</thead>
+				<tbody>
+					{GetStatEntry(false, "Rating: ",   "",  currentAccount, 'elo')}
+					{GetStatEntry(true,  "Win rate: ", "%", currentAccount, 'winRate')}
+					{GetStatEntry(false, "Matches: ",  "",  currentAccount, 'matchesPlayed')}
+					{GetStatEntry(true,  "Wins: ",     "",  currentAccount, 'wins')}
+					{GetStatEntry(false, "Draws: ",    "",  currentAccount, 'draws')}
+					{GetStatEntry(true,  "Losses: ",   "",  currentAccount, 'losses')}
+				</tbody>
+			</table>
+		</div>
+	)
+}
+
+function StatWindow()
+{
+	const { accounts, loggedInAccounts } = useAccountContext();
+	const { indexPlayerStats, showPlayerStats } = useLoginContext();
+	const [ index, setIndex ]  = useState(-1);
+	const [profileImage, setProfileImage] = useState(Player);
+	const [ showStats, setShowStats ] = useState(false);
+
+	const currentAccount = accounts[loggedInAccounts[indexPlayerStats].id - 2];
+	if (currentAccount === null)
+		return;
+
+	return (
+		<AnimatePresence>
+			<motion.div
+				className="fixed inset-0 backdrop-blur-sm flex items-center justify-center z-50 bg-[#1a1a1a]/90"
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				exit={{ opacity: 0 }}
+			>
+				<motion.div
+					className="flex-col items-center bg-[#2a2a2a]/90 backdrop-blur-md text-white p-8 gap-8 rounded-xl w-full min-w-[800px] max-w-[1200px] min-h-[700px] max-w-xl max-h-[700px] relative shadow-2xl"
+					initial={{ scale: 0.9, y: 20 }}
+					animate={{ scale: 1, y: 0 }}
+					exit={{ scale: 0.9, y: 20 }}
+					transition={{ type: "spring", stiffness: 300, damping: 25 }}
+				>
+					{/* // TODO: unfuck exit button */}
+					<button
+						className="absolute top-4 right-4 text-gray-400 hover:text-white hover:bg-neutral-700 p-1 rounded transition"
+						onClick={() => setShowStats(false)}
+					>
+						<IoMdClose size={24} />
+					</button>
+
+					<header className="relative flex items-center justify-center gap-x-2 text-5xl mb-4 border-b border-neutral-700 pb-4">
+						{currentAccount?.username}
+						<img
+							src={profileImage}
+							className="h-14 w-14 rounded-full object-cover shadow-md hover:scale-105 transition-transform duration-300"
+						/>
+					</header>
+
+					<main className="flex h-full gap-x-4 text-2xl text-center">
+						<div className="w-7/20">
+							<ShowStats />
+						</div>
+							
+						<div className="w-13/20">
+							<ShowMatchHistory />
+						</div>
+					</main>
+				</motion.div>
+			</motion.div>
+		</AnimatePresence>
+	);
+}
+
+function PlayerStats()
+{
+	const { accounts, loggedInAccounts } = useAccountContext();
+	const { indexPlayerStats, showPlayerStats } = useLoginContext();
+	const [ index, setIndex ]  = useState(-1);
+	const [ showStats, setShowStats ] = useState(false);
+
+	useEffect(() =>
+	{
+		setIndex(loggedInAccounts[indexPlayerStats].id - 2);
+	}, [ showPlayerStats ]);
+
+	return (
+		<>
+			<motion.button className="pt-2 bg-[#ff914d] px-4 py-2 font-bold shadow-2xl rounded-3xl hover:bg-[#ab5a28] hover:cursor-pointer"
+				whileHover={{ scale: 1.03 }}
+				whileTap={{ scale: 0.97 }}
+				onClick={() => { setShowStats(true) }}>Show stats
+			</motion.button>
+			{showStats && <StatWindow />}
+		</>
+	)
 }
 
 export default PlayerStats
