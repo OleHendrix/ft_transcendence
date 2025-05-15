86c6eaed06af5406a140dd50cb38cd308c87b3d7
Ole Hendrix <160501406+OleHendrix@users.noreply.github.com>
2025-05-15 16:42:06 +0200

    bugfixes

--- frontend/src/user/PlayerInfoProtection.tsx
+++ frontend/src/user/PlayerInfoProtection.tsx
@@ -1,16 +1,23 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { useAccountContext } from "../contexts/AccountContext";
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 function PlayerInfoProtection({ children }: { children: ReactNode }) 
 {
+	const navigate = useNavigate();
 	const { loggedInAccounts } = useAccountContext();
 	const { username } = useParams();
+
+	useEffect(() =>
+	{
+		if (loggedInAccounts.length && !loggedInAccounts.some(account => account.username === username))
+			navigate('/');
+	}, [loggedInAccounts, username, navigate]);
+
 	if (!loggedInAccounts.length)
 		return <div className="text-white text-center mt-10">Loading account...</div>;
-	if (!loggedInAccounts.some(account => account.username === username))
-		throw new Response('Unauthorized', { status: 401 })
-	return children
+
+	return children;
 }
 
 export default PlayerInfoProtection
